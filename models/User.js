eda336c922414e11d8d43c45502c58f4f8ecb17f
Beatriz Ribeiro <contato.biaalves@gmail.com>
2022-02-14 17:44:15 -0300

    ajusta model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-  displayName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  displayName: { type: DataTypes.STRING, validate: { notEmpty: true, len: [8, 200] } },
+  email: {
+  type: DataTypes.STRING,
+  validate: {
+  notEmpty: true,
+  isEmail: true,
+  },
+  },
+  password: { type: DataTypes.STRING, validate: { notEmpty: true, len: [6, 6] } },
+  image: DataTypes.STRING,
   },
   {
   timestamps: false,
   tableName: 'Users',
   });
-
+  
   return User;
-  };  
+  }; 
