38b9c4bdf2b782b672c8afc96f1b1947c1577ef8
Beatriz Ribeiro <contato.biaalves@gmail.com>
2022-02-17 20:41:41 -0300

    Requsiito 2 - Tá rolando!!!!

--- routers/loginRoute.js
+++ routers/loginRoute.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 const { userLogin } = require('../controllers/loginController');
+const { loginValidation } = require('../middlewares/validations');
 
-router.post('/', userLogin);
+router.post('/', loginValidation, userLogin);
 
 module.exports = router;
