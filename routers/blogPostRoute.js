da800e6ad29ed1973dcb082398d54a92ca772979
Beatriz Ribeiro <contato.biaalves@gmail.com>
2022-02-24 16:42:46 -0300

    Cria rota para editar um post

--- routers/blogPostRoute.js
+++ routers/blogPostRoute.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const { authentication } = require('../middlewares/authentication');
-const { createPost, getAllPosts, getPostByPk } = require('../controllers/blogPostController');
+const {
+  createPost, getAllPosts, getPostByPk, update } = require('../controllers/blogPostController');
 const { blogPostValidation, categoryValidation } = require('../middlewares/validations');
+const { categoryEditableValidation, userValidation } = require('../middlewares/postValidation');
 
 router.post('/',
   authentication,
@@ -17,5 +19,12 @@ router.get('/:id',
   getPostByPk);
 
 router.get('/', authentication, getAllPosts);
+
+router.put('/:id',
+  authentication,
+  blogPostValidation,
+  categoryEditableValidation,
+  userValidation,
+  update);
 
 module.exports = router;
